e61947103f618a0724ced860aeff13ee740e11ce
ahmed-razic <Ibrahim2012*>
2021-02-07 21:40:25 +0100

    finished

--- app/src/main/java/com/example/android/findstoreapp/MainActivity.java
+++ app/src/main/java/com/example/android/findstoreapp/MainActivity.java
@@ -1,14 +1,48 @@
 package com.example.android.findstoreapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ShareCompat;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText storeName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        storeName = findViewById(R.id.store_name_editText);
+
+    }
+
+    public void findStore(View view) {
+        String address = "geo:0,0?q=" + storeName.getText().toString();
+        Uri addressUri = Uri.parse(address);
+        Intent intent = new Intent(Intent.ACTION_VIEW, addressUri);
+        startActivity(intent);
+
+/*        if(intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        } else {
+            Toast.makeText(this, "Do not work", Toast.LENGTH_LONG).show();
+        }*/
     }
 }
+
+
+/*
+    public void findStore(View view) {
+        String loc = storeName.getText().toString();
+        Uri addressUri = Uri.parse("geo:0,0?q=" + loc);
+        Intent intent = new Intent(Intent.ACTION_VIEW, addressUri);
+
+        startActivity(intent);
+    }*/
